70d039666c287c8031b40163b08f5516f7364c46
Usukh <uerdeneochi@cps.edu>
2024-12-09 11:31:59 -0600

    1

--- part1-linear-regression/a6_part1.py
+++ part1-linear-regression/a6_part1.py
@@ -10,15 +10,7 @@ y = data["Blood Pressure"].values
 # Use reshape to turn the x values into 2D arrays:
 x = x.reshape(-1,1)
 
-# Create the model
-
-# Find the coefficient, bias, and r squared values. 
-# Each should be a float and rounded to two decimal places. 
-
-
-# Print out the linear equation and r squared value
-
-# Predict the the blood pressure of someone who is 43 years old.
-# Print out the prediction
-
-# Create the model in matplotlib and include the line of best fit
+model= LinearRegression().fit(x,y)
+coef= (round(float(model.coef_[0])),2)
+intercept = round(float(model.intercept), 2)
+r_squared = model.score(x,y)

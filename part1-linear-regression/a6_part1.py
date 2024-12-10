6158df76537808adc7044e327aca030f68790026
Usukh <uerdeneochi@cps.edu>
2024-12-10 11:09:56 -0600

    P1 completed

--- part1-linear-regression/a6_part1.py
+++ part1-linear-regression/a6_part1.py
@@ -7,10 +7,32 @@ data = pd.read_csv("part1-linear-regression/blood_pressure_data.csv")
 x = data["Age"].values
 y = data["Blood Pressure"].values
 
-# Use reshape to turn the x values into 2D arrays:
+
 x = x.reshape(-1,1)
 
 model= LinearRegression().fit(x,y)
 coef= (round(float(model.coef_[0])),2)
 intercept = round(float(model.intercept), 2)
 r_squared = model.score(x,y)
+x_predict = 43
+prediction = model.predict([[x_predict]])
+
+
+print(f"Model's Linear Equation: y = {coef}x + {intercept}")
+print(f"R Squared value: {r_squared}")
+print(f"Prediction when x is {x_predict}: {prediction}")
+
+
+plt.figure(figsize=(6,4))
+
+plt.scatter(x,y, c="purple")
+plt.scatter(x_predict, prediction, c="blue")
+
+plt.xlabel("Temperature °F")
+plt.ylabel("Chirps per Minute")
+plt.title("Cricket Chirps by Temperature")
+
+plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
+
+plt.legend()
+plt.show()

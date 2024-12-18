7bfd71a20a02d09f05d1d7b6cb660cd6f1794d0b
Usukh <uerdeneochi@cps.edu>
2024-12-18 11:44:49 -0600

    Completed part 3

--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -7,16 +7,21 @@ from sklearn.model_selection import train_test_split
 data = pd.read_csv("part3-multivariable-linear-regression/car_data.csv")
 x = data[["miles","age"]].values
 y = data["Price"].values
-
-#split the data into training and testing data
-
-#create linear regression model
-
-#Find and print the coefficients, intercept, and r squared values. 
-#Each should be rounded to two decimal places. 
-
-
-#Loop through the data and print out the predicted prices and the 
-#actual prices
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
+print(xtrain)
+print(xtest)
+model = LinearRegression().fit(xtrain, ytrain)
+coef = np.around(model.coef_, 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = round(model.score(x, y),2)
+print(f"Model's Linear Equation: y={coef[0]}x1 + {coef[1]}x2 + {intercept}")
+print("R Squared value:", r_squared)
 print("***************")
 print("Testing Results")
+xtest=[[252000,10]]
+print(xtest)
+predict=model.predict(xtest)
+predict = np.around(predict, 2)
+for i in range(len(xtest)):
+    xCoord=xtest[i]
+    print(f"Miles: {xCoord[0]} Age: {xCoord[1]} Predicted Price:{int(predict[i])}")
--- part3-multivariable-linear-regression/a6_part3_writeup.md
+++ part3-multivariable-linear-regression/a6_part3_writeup.md
@@ -5,9 +5,10 @@ After completing `a6_part3.py` answer the following questions
 ## Questions to answer
 
 1. What is the R Squared coefficient for your model? What does that mean about the model in relation to your data?
-
+# The r square value is 0.87. This tells me the correlation between the the x value and the resulting y value.
 2. Is your model accurate? Why or why not?
-
+# Yes because it is above 0.75, which means that it is considered to be accurate.
 3. What does the model predict a 10-year-old car with 89000 miles is worth? What about a car that is 20 years old with 150000 miles?
-
+# A 10-year old care with 89,000 miles is worth $8990 and a 20 year old care with 150,000 is wprth $1,000.
 4. You may notice that some of your predicted results are negative. This is occurring when the value of age and the mileage of the car are very high. Why do you think this is happening?
+# This is due to a lack of data. Cares with ridiculously high mileages and ages can not be properly be predicted with this data set.

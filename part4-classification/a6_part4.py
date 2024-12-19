3be6077ccf027c0d7d07a1efcb5161ca5de6963b
Usukh <uerdeneochi@cps.edu>
2024-12-19 10:57:27 -0600

    Completed Part 4 and fixed a typo in p2 writeup

--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -10,18 +10,26 @@ x = data[["Age", "EstimatedSalary", "Gender"]].values
 y = data["Purchased"].values
 
 # Step 1: Print the values for x and y
-
+print(x)
+print(y)
 # Step 2: Standardize the data using StandardScaler, 
-
+m = StandardScaler().fit(x)
 # Step 3: Transform the data
-
+x = m.transform(x)
 # Step 4: Split the data into training and testing data
-
-# Step 5: Fit the data
-
+xtrn, xtst, ytrn, ytst = train_test_split(x, y)
 # Step 6: Create a LogsiticRegression object and fit the data
-
+model = linear_model.LogisticRegression().fit(xtrn, ytrn)
 # Step 7: Print the score to see the accuracy of the model
-
+score=model.score(xtst,ytst)
+print(score)
 # Step 8: Print out the actual ytest values and predicted y values
 # based on the xtest data
+for i in range(len(xtst)):
+    x=m.inverse_transform(xtst)
+    if x[i][2]==0:
+        Gender="Male"
+    else:
+        Gender="Female"
+    if ytst[i]!=y[i]:
+        print(f"Gender:{x[i][2]} Salary:{int(x[i][1])} Age:{int(x[i][0])} Purchased(predicted):{y[i]} Purchased(real):{ytst[i]}")
--- part4-classification/a6_part4_writeup.md
+++ part4-classification/a6_part4_writeup.md
@@ -5,10 +5,10 @@ After completing `a6_part4.py` answer the following questions
 ## Questions to answer
 
 1. Comment out the StandardScaler and re-run your test. How accurate is the model? Why is that?
-
+# This model is less accurate because it is very skewed.
 2. How accurate is the model with the StandardScaler? Is this model accurate enough for the given use case? Explain.
-
+# I think it is somewhat accurate but there are better options.
 3. Looking at the predicted and actual results, how did the model do? Was there a pattern to the inputs that the model was incorrect about?
-
+# A good amount of the errors were in the middle age range.
 4. Would a 34 year old Female who makes 56000 a year buy an SUV according to the model? Remember to scale the data before running it through the model.
-
+# No she will not.
